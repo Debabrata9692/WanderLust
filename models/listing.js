99dcd967a9af4d91c139f700a7eea2440e91d5ba
Debabrata Sahoo <debabratasahoo526@gmail.com>
2025-11-19 15:31:53 +0530

    Add Project Files

---
+++ models/listing.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const review = require("./review");
+const { string } = require("joi");
+const Schema = mongoose.Schema;
+const listeningSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+listeningSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listeningSchema);
+module.exports = Listing;
